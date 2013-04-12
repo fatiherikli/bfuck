6d0c18e2d57561625076fcc75e32df7211e6a5e1
fatiherikli <fatiherikli@gmail.com>
2013-04-13 02:23:14 +0300

    Added readme

--- bfuck.py
+++ bfuck.py
@@ -26,7 +26,6 @@ def run(source, tape):
         action += 1
 
 run("""
-
 +++++ +++++             initialize counter (cell #0) to 10
 [                       use loop to set the next four cells to 70/100/30/10
     > +++++ ++              add  7 to cell #1
